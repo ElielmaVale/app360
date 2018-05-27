e517fc4c76b53eb1c4a5f93ccd4cc2e76df75786
Alex Cavalheiro <souzacavalheiro@hotmail.com>
2018-05-27 10:25:26 -0300

    projeto deszip

---
+++ BELÉM360º/belem360app/app/src/main/java/com/example/souza/belem360app/tela1.java
@@ -0,0 +1,14 @@
+package com.example.souza.belem360app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_tela1);
+    }
+}
